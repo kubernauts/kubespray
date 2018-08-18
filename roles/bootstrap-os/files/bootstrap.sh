f234caf1f7d45df097c66a6ddda34255e1e829a0
Manuel Müller <mueller.m.h@gmail.com>
2018-08-18 16:27:20 +0200

    Bug/coreos (#2)
    * fix ubuntu setup
    
    Signed-off-by: Manuel Müller <mueller.m.h@gmail.com>
    
    * fix coreos
    
    Signed-off-by: Manuel Müller <mueller.m.h@gmail.com>

--- roles/bootstrap-os/files/bootstrap.sh
+++ roles/bootstrap-os/files/bootstrap.sh
@@ -14,15 +14,19 @@ fi
 PYPY_VERSION=5.1.0
 
 wget -O - https://bitbucket.org/pypy/pypy/downloads/pypy-$PYPY_VERSION-linux64.tar.bz2 |tar -xjf -
-mv -n pypy-$PYPY_VERSION-linux64 pypy
+mv -n pypy-$PYPY_VERSION-linux64 $BINDIR/pypy
 
 ## library fixup
 mkdir -p pypy/lib
+<<<<<<< bug/coreos
+ln -snf /lib64/libncurses.so.6.1 $BINDIR/pypy/lib/libtinfo.so.5
+=======
 if [ -f /lib64/libncurses.so.5.9 ]; then
   ln -snf /lib64/libncurses.so.5.9 $BINDIR/pypy/lib/libtinfo.so.5
 elif [ -f /lib64/libncurses.so.6.1 ]; then
   ln -snf /lib64/libncurses.so.6.1 $BINDIR/pypy/lib/libtinfo.so.5
 fi
+>>>>>>> master
 
 cat > $BINDIR/python <<EOF
 #!/bin/bash
